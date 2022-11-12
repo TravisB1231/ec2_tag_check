3231003fd091080e711902ed060a00e8ab76f55e
Travis Brown <travisrbrown1231@gmail.com>
2022-11-12 13:26:33 -0500

    Added tracking for Olympus_do_not_stop tag

--- ec2_instance.py
+++ ec2_instance.py
@@ -1,8 +1,8 @@
 """Data class for EC2 instance data."""
 class Ec2Instance:
     """
-    Filters out irrelevant boto3 EC2 data. Makes tag data easier to work with.
-    Adds custom EC2 data.
+    Filters out irrelevant boto3 EC2 data. Adds custom/enriches current EC2 data.
+    Makes tag data easier to work with.
     """
     def __init__(self, instance_data:dict):
         self.id = instance_data["InstanceId"]
@@ -15,23 +15,20 @@ class Ec2Instance:
             self.os = "Ubuntu"
         self.instance_name = None
         self.instance_email = None
+        self.do_not_stop = False
         self.tags = self._enumerate_instance_tags(instance_data["Tags"])
 
     def _enumerate_instance_tags(self, tags_list:list) -> dict:
         tags = {}
-        # TODO: dict comprehension
         for tag in tags_list:
-            tag_key = tag["Key"]
+            tag_key = tag["Key"].lower()
             tag_val = tag["Value"]
             tags[tag_key] = tag_val
-            if tag_key == "Name":
+            if tag_key == "name":
                 self.instance_name = tag_val
-            elif tag_key == "Owner_Email":
+            elif tag_key == "owner_email":
                 self.instance_email = tag_val
+            elif tag_key == "olympus_do_not_stop":
+                self.do_not_stop = True
         return tags
-"""
-TODO: make new string from Owner_Email tag value and strip everything
-    from and after '@' afterward:
-    owner_name = [Owner_Email][0] + [Owner_Email][1:]
-"""
     
